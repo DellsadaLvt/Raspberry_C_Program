ba9eab1f0ded8db747a5d91022029f2797c3fdfd
DellsadaLvt <levantho0168@gmail.com>
2021-03-19 17:14:02 +0700

    upload_01

--- Class_code.c
+++ Class_code.c
@@ -2,9 +2,20 @@
 #include<wiringPi.h>
 
 
+#define red		12   // 12 is the physical in rasp
+#define blue	16
+#define green	18
+
 int main(void)
 {
-	printf("hell world");
+	//setup lib
+	wiringPiSetupPhys();
+	
+	// setup IO
+	
+	// initial state
+	
+	
 	
 	
 	
