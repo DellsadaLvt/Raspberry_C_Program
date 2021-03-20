7d55034b754313ecdb9e5a9f21323590f9c92355
DellsadaLvt <levantho0168@gmail.com>
2021-03-20 17:38:39 +0700

    modify 00

--- ex_04.c
+++ ex_04.c
@@ -12,13 +12,10 @@
 
 // INPUT
 #define button_00	11
-#define button_01	13
-#define button_02	15
-#define button_03	7
+
 
 
 /* variables */
-short signed int mode, light;
 unsigned int arrayLed[4] = { 12, 18, 16 };
 unsigned int timez;
 
@@ -26,41 +23,6 @@ unsigned int timez;
 // function declare
 void lighLedRandom( void );
 
-
-
-/* INTERRUPTS FUNC */
-void handle_button_00( void )
-{
-	/* if( 1 == digitalRead(button_00) )
-	{
-		delay(10);
-		if( 1 == digitalRead(button_00) )
-		{
-			mode++;
-			if( 2 < mode )
-				mode = 0;
-			printf("Mode: %d \n", mode);
-			printf("Light level: %d \n", light);
-		}
-	} */
-}
-/*******************/
-void handle_button_01( void )
-{
-	
-}
-/*****************************/
-void handle_button_02( void )
-{
-	
-}
-/**************************************/
-void handle_button_03( void )
-{
-	
-	
-}
-	
 
 
 
@@ -76,27 +38,16 @@ int main( void )
 	pinMode(	blue, 	OUTPUT	);
 	pinMode(	green, 	OUTPUT	);
 	pinMode(	button_00,	INPUT	);
-	pinMode(	button_01,	INPUT	);
-	pinMode(	button_02, 	INPUT	);
-	pinMode(	button_03, 	INPUT	);
+	
 	
 	
 	// INTERRUPTS
-	wiringPiISR(	button_00, INT_EDGE_RISING, &handle_button_00	);
-	wiringPiISR(	button_01, INT_EDGE_RISING, &handle_button_01	);
-	wiringPiISR(	button_02, INT_EDGE_RISING, &handle_button_02	);
-	wiringPiISR(	button_03, INT_EDGE_RISING, &handle_button_03	);
+//wiringPiISR(	button_00, INT_EDGE_RISING, &handle_button_00	);
+
 	
-	
-	// SETUP SOFTPWM
-	softPwmCreate(	red, 	0, 100	);
-	softPwmCreate(	blue,	0, 100	);
-	softPwmCreate(	green,	0, 100	);
 	
 	
 	// INITIAL STATE
-	mode	= 	0;
-	light	=	0;
 	timez	=	0;
 	
 	
