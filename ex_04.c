bb4e3b4b5d6ee244ffd133b945fc9337bd2efe27
DellsadaLvt <levantho0168@gmail.com>
2021-03-25 15:52:51 +0700

    complete the code

--- ex_04.c
+++ ex_04.c
@@ -6,21 +6,49 @@
 
 // INPUT
 #define button_00	11
+#define button_01	13
+#define button_02	15
+#define button_03	7
 
 
 
 /* variables */
-unsigned int arrayLed[4] = { 12, 18, 16 };
-unsigned int timez;
+unsigned int arrayLed[4] = { 12, 18, 16 }; // 12 ==> red, 18 ==> green , 16 ==> blue
+volatile unsigned int count;
+unsigned int timez, led;
+_Bool mode;
 
 
-// function declare
+
+/****************************** DECLARE FUNCTION *************************************/
+void controlLed( _Bool x);
 void lighLedRandom( void );
+/* x is the button, y is the position of led in array led */
+void interrupts_handle_button( char x , char y );
 
 
 
+// interrupts function
+void handle_button_00( void )
+{
+	interrupts_handle_button(button_00, 0); 
+}
+/****************************/
+void handle_button_01( void )
+{
+	interrupts_handle_button( button_01, 1);
+}
 
-// MAIN FUNCTION
+/************************/
+void handle_button_02( void )
+{
+	interrupts_handle_button( button_02, 2);
+}
+//
+//
+//
+//
+/****************************** MAIN FUNCTION *************************************/
 int main( void )
 {
 	// SETUP LIB
@@ -28,20 +56,35 @@ int main( void )
 	
 	
 	// SETUP IO
-	pinMode(	red , 		OUTPUT	);
-	pinMode(	blue, 		OUTPUT	);
-	pinMode(	green, 		OUTPUT	);
+	for(int i=0; i<3; i++)
+		pinMode(	arrayLed[i], OUTPUT	);
 	pinMode(	button_00,	INPUT	);
+	pinMode(	button_01,	INPUT	);
+	pinMode(	button_02, 	INPUT	);
+	pinMode(	button_03, 	INPUT	);
+
+	
+	
+	
+	// INTERRUPTS
+	wiringPiISR( button_00, INT_EDGE_RISING, &handle_button_00 );
+	wiringPiISR( button_01, INT_EDGE_RISING, &handle_button_01 );
+	wiringPiISR( button_02, INT_EDGE_RISING, &handle_button_02 );
 	
 	
 	// INITIAL STATE
 	timez	=	0;
+	mode = 1;
+	count = 0;
+	for( int i=0; i<3; i++)
+		digitalWrite( arrayLed[i], 0);
 	
 	
-	// LOOP 
+	/********************* LOOP **************************/
 	while( 1 )
 	{
 		lighLedRandom();
+		
 	}
 	
 	
@@ -49,24 +92,67 @@ int main( void )
 }
 
 
-/********************* subroutine *****************/
+/********************* SUBROUTINE *****************/
 
 void lighLedRandom( void )
 {
-	if( millis() - timez >=	3000	)
+	if(		millis() - timez >=	3000	)
 	{
 		timez = millis();
 		
 		// on led random
-		digitalWrite( arrayLed[rand()%3] , 1);
+		led = rand()%3;
+		digitalWrite( arrayLed[led] , 1);
 		
+		
+		/* change mode */
+		mode = 1; 									// true is allow press button
+		
+		
+		/* print count */
+		printf(" So lan nhan: %d \n", count);
 		delay(500);
 		
 		// turn all led
-		digitalWrite( red,	0);
-		digitalWrite( blue,  0);
-		digitalWrite( green, 0);
+		controlLed(0);
+		
+		
+		/* unenable button */
+		mode = 0; 									// false is not allow press button
+	}
+	
+}
 
+void controlLed( _Bool x)
+{
+	for( int i = 0; i< 3; i++  )
+	{
+		digitalWrite(arrayLed[i], x);
 	}
 }
+/* x is the button, y is the position of led in array led */
+void interrupts_handle_button( char x , char y )
+{
+	if( 1 == mode )
+		if( y == led )
+			if( 1 == digitalRead( x ) )
+			{
+				delay(20);
+				if( 1 == digitalRead( x ) )
+				{
+					// create the time of press button
+					count++;
+					
+					for( int j=0; j< 2; j++)
+					{
+						controlLed(1);
+						delay(250);
+						controlLed(0);
+						delay(250);				
+					}
+					
+				}
+			} 
+}
+
 
