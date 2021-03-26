bd8842d3d7ecc3c722fd222c3d8af62d0f621ec0
DellsadaLvt <levantho0168@gmail.com>
2021-03-26 15:47:05 +0700

    complete code ex_05

--- ex_05.c
+++ ex_05.c
@@ -5,44 +5,63 @@
 #include<stdlib.h>
 
 
-/********************** INPUT **********************/
-#define button_00	11
-#define button_01	13
-#define button_02	15
+/*********************** INPUT **********************/
+#define button_00	11				// use when red led high
+#define button_01	13				// settup soLanSet
+#define button_02	15				// reset all
 #define button_03	7
 
 
 
 /********************** OUTPUT **********************/
-unsigned int arrayLed[4] = { 12, 18, 16 }; // 12 ==> red, 18 ==> green , 16 ==> blue
-volatile unsigned int arrayScore[12];
+unsigned 	 		char arrayLed[4] = { 12, 18, 16 }; 	// 12 ==> red, 18 ==> green , 16 ==> blue
+volatile unsigned 	int  arrayScore[12];					// store time when press button_00
 
 
 /********************** VARIABLES **********************/
 //volatile unsigned int 
 //unsigned int time;
-unsigned int time_set, time;
-volatile unsigned int time_press;
-_Bool flag_mode, flag_annouce;
-unsigned int soLan, avg;
+unsigned 			int			time_set, time;
+volatile unsigned 	int 		time_press;
+_Bool 							flag_mode, flag_annouce;
+unsigned 			char 		soLan, soLanSet;
 
 
 
-/****************************** DECLARE FUNCTION *************************************/
-void random_lighting ( void );
-void interrupts_handle_button( char x );
-void loop_time( void );
-void blink( void );
-void avg_func( void );
+/******************** DECLARE FUNCTION ***************************/
+void 	random_lighting( void );
+void 	interrupts_handle_button( char x );
+void 	loop_time( void );
+void 	blink( void );
+float	avg_func( char soPhanTu );
+void 	reset_all( void );
 
 
 
-/********************** INTERRUPTS FUNCTION **********************/
+/******************* INTERRUPTS FUNCTION ********************/
 void handle_button_00( void )
 {
-	 interrupts_handle_button( button_00);
+	interrupts_handle_button( button_00);
 }
 /****************************/
+void handle_button_02( void )
+{
+	if( soLan >= soLanSet )
+		if( 1 == digitalRead( button_02 ) )
+		{
+			delay(20);
+			if( 1 == digitalRead( button_02 ) )
+			{
+				for ( int i = 0; i < 12; i++)
+					arrayScore[i] = 0;
+				blink();
+				delay(2000);
+				soLan = 0;
+			}
+			
+		}
+}
+/**********************/
 
 //
 //
@@ -56,46 +75,22 @@ int main( void )
 	
 	
 	// SETUP IO
+	//led
 	for(int i=0; i<3; i++)
 		pinMode(	arrayLed[i], OUTPUT	);
-	pinMode(	button_00,	INPUT	);
+	//button
+		pinMode(	button_00,	INPUT	);
 
 
 
 	// INTERRUPTS
 	wiringPiISR( button_00, INT_EDGE_RISING, &handle_button_00 );
+	//wiringPiISR( button_02, INT_EDGE_RISING, &handle_button_01 );
+	wiringPiISR( button_02, INT_EDGE_RISING, &handle_button_02 );
 	
 	
-	
-	// INITIAL STATE
-	time = 0;
-	time_set = 1000;
-	flag_mode = 0; 
-	flag_annouce = 0;
-	time_press = 0;
-	soLan = 0;
-	avg = 0;
-	
-	
-	// 0 value is unenable press button
-	/* turn off all led */
-	for(int i=0; i<3; i++)
-		digitalWrite(	arrayLed[i], 0	);
-	
-	for ( int i = 1; i < 11; i++)
-	{
-		arrayScore[i] = 0;
-		//printf("pt %d\n", arrayScore[i]);	
-	}
-	
-	for( int i = 0; i<2; i++)
-	{
-		digitalWrite(arrayLed[0], 1);
-		delay(200);
-		digitalWrite(arrayLed[0], 0);
-		delay(100);
-	}
-	
+	// reset all
+	reset_all();
 	
 	/********************* LOOP **************************/
 	while( 1 )
@@ -113,25 +108,23 @@ int main( void )
 /* USE RANDOM LIGHTING 10 TIMES */
 void loop_time( void )
 {
-	while( soLan < 4)
+	while( soLan < soLanSet)
 	{
-		random_lighting();
-		soLan++;
-		if( soLan >=4)
-			avg_func();
+		random_lighting();				
 		delay(2000);
+		soLan++;
+		if( soLan >= soLanSet)	
+			printf(" the result is: %0.2f \n", (avg_func(soLanSet)));
 	}
-	
-	
-	
 }
 
 
 /* RANDOM LIGHTING */
 void random_lighting ( void )
 {	
-	
 	 /* LIGHTING RANDOM */
+	while( (millis() - time) < (time_set));
+	
 	if( (millis() - time) >= (time_set) )
 	{
 		//printf( "      time set is: %d \n" , time_set);
@@ -145,13 +138,14 @@ void random_lighting ( void )
 		{	
 			flag_mode = 0;			// 0 is unenable press button
 			digitalWrite(arrayLed[0], 0);
-			printf( "goodluck later! \n ");
+			printf("goodluck later!\n");
 		}
 		
 		time_set = (rand()%5)*1000;
 		//delay(3000);
 		//printf("time set: %d \n" , time_set);
 	} 
+	
 }
 
 
@@ -170,7 +164,7 @@ void interrupts_handle_button( char x  )
 				flag_mode = 0;
 				digitalWrite( arrayLed[0], 0);
 				arrayScore[soLan] = millis() - time;
-				printf( "time press [%d]: %d \n ", soLan,arrayScore[soLan]);
+				printf("time press [%d]: %d\n", soLan,arrayScore[soLan]);
 				
 			}
 		} 
@@ -179,23 +173,60 @@ void interrupts_handle_button( char x  )
 /* blink func */
 void blink (void)
 {
+	for(int i = 0; i<2 ; i++)
+	{
 		digitalWrite(arrayLed[0], 1);
 		delay(200);
 		digitalWrite(arrayLed[0], 0);
 		delay(100);
+	}
 }
 
 /* average function */
-void avg_func( void )
+float avg_func( char soPhanTu )
 {
-	float kq;
-	int i;
-	for(  i = 1; i< 3; i++)
-	{
+	float result = 0;
+	/* caculate sum of all components */
+	for( char i = 0; i< (soPhanTu); i++)
 		arrayScore[i+1] = arrayScore[i+1] + arrayScore[i] ;
-		kq = arrayScore[i+1];
-		//printf("array[i]    : %d \n", arrayScore[i+1] );
+	
+	
+	//printf("array[i]    : %d \n", arrayScore[i+1] );
+	//printf("average: %0.6f \n", kq );
+	result = (float)arrayScore[(soPhanTu)]/soPhanTu;
+	
+	return (result);
+}
+
+
+
+/* reset all func */
+void reset_all( void )
+{
+	/* turn off all led */
+	for(int i=0; i<3; i++)
+		digitalWrite(	arrayLed[i], 0	);
+	
+	/* set all value in arrayScore is 0 */
+	for ( int i = 0; i < 12; i++)
+	{
+		arrayScore[i] = 0;
+		//printf("pt %d\n", arrayScore[i]);	
 	}
-	kq = kq/3;
-	printf("average: %0.6f \n", kq );
+	
+	// INITIAL STATE
+	time 			= 	0	;		// time = millis();
+	time_set 		= 	1000;		// setup time lighting
+	flag_mode 		= 	0	; 		// flag_mode: 0 :value is unenable press button
+	flag_annouce 	= 	0	;		// flag_annouce: 0 : is unenable printf
+	time_press		= 	0	;		// caculate time from led high to press butoon
+	soLan 			= 	0	;		// the counter in while loop and the position in arrayScore
+	soLanSet 		= 	3	;		// setup the time while loop
+	
+	
+	blink();
+	
+	
+	printf("RESET ALL\n");
+	delay(1000);
 }
