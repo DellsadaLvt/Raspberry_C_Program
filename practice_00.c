1cc7fa8be41cdd4e56001d906259984955361d09
DellsadaLvt <levantho0168@gmail.com>
2021-04-26 17:38:24 +0700

    Read Pitch value by Interrupts

--- practice_00.c
+++ practice_00.c
@@ -9,7 +9,7 @@
 
 
 /*======================= DEFINE =======================*/
-#define int_mpu 		7			// the pin int of mpu
+#define int_mpu 		11			// the pin int of mpu
 #define Samp_rate		25			// 0x01: SMPLRT_DIV[7:0] is 1 ==> the sample rate: 500Hz
 #define Config			26			// 0x02: FSYNC( input disable ), DLPF ( MODE 2)
 #define Gyro_config 	27			// 0x08: no Self-test, full scale: 1 ==> +-500 do/sample
@@ -18,8 +18,11 @@
 #define Acc_x_out		59	
 #define Acc_y_out		61
 #define Acc_z_out		63
-#define Ena_int 		56
+#define Int_config		55			// 0x80: int level is low active in data readly.
+#define Ena_int 		56			// 0x01: set interrupts in only data readly mode
 
+/*======================== VARIABLES ==================*/
+int mpu;
 /*======================= DECLARE FUNC =======================*/
 void test_func( void);
 void reset_all_register(const int *mpu);
@@ -29,6 +32,22 @@ void test_function(const int *mpu, const int8_t reg_add, int8_t value);
 int16_t read_Acc(const int *mpu, uint8_t sensor_add);
 float caculate_angle(const int *mpu, char mode[]);
 
+
+/*======================= INTERRUPTS FUNCTION =======================*/
+void handle_int_mpu( void )
+{
+	
+	//printf("the int status is: %x\n", u8ReadIntStatus); 
+	float read = caculate_angle( &mpu, "pitch");
+	//int16_t test = read_Acc( &mpu, Acc_x_out);
+	printf("The Pitch value: %.3f\n", read); 	
+	//int16_t rusult = read_Acc( &mpu, Acc_x_out)
+	//int8_t u8ReadIntStatus = wiringPiI2CReadReg8( mpu, 58);
+	delay(500);
+
+}
+
+
 /*======================= MAIN FUNCTION =======================*/
 int main( void )
 {
@@ -36,11 +55,14 @@ int main( void )
 	wiringPiSetupPhys();
 	
 	/* INITIAL I2C */
-	int mpu = wiringPiI2CSetup(0x68);
+	mpu = wiringPiI2CSetup(0x68);
 	//printf("\nThe mpu value: %x\n", mpu);
 	
 	/* SETUP IO */
 	pinMode( int_mpu, INPUT);
+	
+	/* CREATE INTERRUPTS */
+	wiringPiISR( int_mpu, INT_EDGE_RISING, &handle_int_mpu);
 	
 	/* RESET ALL REGISTER */
 	//reset_all_register( &mpu );
@@ -48,17 +70,21 @@ int main( void )
 	/*  INITIAL MPU*/
 	init_mpu(&mpu);
 	
+	//int8_t r = wiringPiI2CReadReg8( mpu, 58);
+	
+	
 	/*-------- WHILE LOOP ---------*/
 	while(1)
 	{
-		float read = caculate_angle( &mpu, "pitch");
-		//int16_t test = read_Acc( &mpu, Acc_x_out);
-		printf("the read sensor: %f\n", read);
-		delay(500); 	
+
 	}
 	
 	return 0;
 }
+
+
+
+
 
 
 /*================ SUBROUTINE ==========================*/
@@ -75,7 +101,8 @@ void init_mpu(const int *mpu )
 	wiringPiI2CWriteReg8(*mpu, Config, 2);
 	wiringPiI2CWriteReg8(*mpu, Gyro_config, 0x08);
 	wiringPiI2CWriteReg8(*mpu, Acc_config, 0x08);
-	//wiringPiI2CWriteReg8(*mpu, Ena_int, 0x01);
+	wiringPiI2CWriteReg8(*mpu, Int_config, 0);
+	wiringPiI2CWriteReg8(*mpu, Ena_int, 0x01);
 	wiringPiI2CWriteReg8(*mpu, Power_manager, 0x01);
 	
 	printf("\nComplete config mpu\n"); 
@@ -103,7 +130,8 @@ int16_t read_Acc( const int *mpu, uint8_t sensor_add)
 {
 			int16_t high	= wiringPiI2CReadReg8(*mpu, sensor_add);
 			int16_t low		= wiringPiI2CReadReg8(*mpu, (sensor_add+1));
-			int16_t Acc		= (high<<8) | low;
+			int16_t	Acc 	= (high << 8) |low;
+			//printf("the result: %x     %d\n", Acc, Acc);
 			return 	Acc;	
 }
 
@@ -126,7 +154,7 @@ float caculate_angle( const int *mpu, char mode[] )
 		return pitch;
 	}
 	else
-		printf("\nOut value\n");
+		printf("\nThe value invalid\n");
 	
 }
 
